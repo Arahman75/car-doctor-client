ac6a748781c3e8ff4c0822bacd01be0c06d5bf84
Arahman75 <selfarahman01825@gmail.com>
2023-11-02 10:41:07 +0600

    added a service confirm system

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -12,7 +12,48 @@ const Bookings = () => {
             .then(res => res.json())
             .then(data => setBookings(data)
             )
-    }, [])
+    }, [url]);
+
+    const handleDelete = (id) => {
+        const proceed = confirm('Are you sure you want to delete');
+        if (proceed) {
+            fetch(`http://localhost:5000/bookings/${id}`, {
+                method: 'DELETE',
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.deletedCount > 0) {
+                        alert('Deleted successfully')
+                        const remaining = bookings.filter(booking => booking._id !== id);
+                        setBookings(remaining)
+                    }
+                })
+        }
+    }
+
+    const handleBookingConfirm = (id) => {
+        fetch(`http://localhost:5000/bookings/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ status: 'confirm' })
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount > 0) {
+                    alert('user confirm successfully');
+                    const remaining = bookings.filter(booking => booking._id !== id);
+                    const update = bookings.find(booking => booking._id === id);
+                    update.status = 'confirm';
+                    const newBooking = [update, ...remaining];
+                    setBookings(newBooking);
+                }
+            })
+    }
+
     return (
         <div>
             <h1 className='text-3xl'>Bookings : {bookings.length}</h1>
@@ -38,6 +79,8 @@ const Bookings = () => {
                             bookings.map(booking => <BookingRow
                                 booking={booking}
                                 key={booking._id}
+                                handleDelete={handleDelete}
+                                handleBookingConfirm={handleBookingConfirm}
                             ></BookingRow>)
                         }
                     </tbody>
